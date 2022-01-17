a6d9d9d130b1d5fa4d3576f54fd74af0003c2d0c
CodingPuffin007 <tirthcamocode.org@gmail.com>
2022-01-17 20:20:42 +0530

    added login system

--- Components/SideBar.jsx
+++ Components/SideBar.jsx
@@ -6,13 +6,21 @@ import {
     HeartIcon,
     RssIcon
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 
 
 function SideBar() {
+    const { data: session, status } = useSession()
+
     return (
         <div className='text-gray-500 p-5 text-sm border-gray-900'>
             <div className='space-y-4'>
+            <button className='flex items-center space-x-2 
+                hover:text-white'
+                onClick={() => signOut()}>
+                    <p>Log Out</p>
+                </button>
                 <button className='flex items-center space-x-2 
                 hover:text-white'>
                     <HomeIcon className="w-5 h-5"/>
